a2701d7367fac18743723ba5828f381702d3e8b0
samaysar <samay.sar@gmail.com>
2024-02-11 16:21:29 +0100

    refactoring out text project

--- DevFast.Net.Text/src/DevFast.Net.Text/Json/NamespaceDoc.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DevFast.Net.Text.Json
-{
-    /// <summary>
-    /// Under this <see langword="namespace"/> various JSON data related artifacts are implemented.
-    /// </summary>
-    internal static class NamespaceDoc;
-}
